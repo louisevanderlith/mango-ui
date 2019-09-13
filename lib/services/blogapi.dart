0255b8c6ecc652fd3fe9d7fcdd67818922d7160f
Louise van der Lith <louisevanderlith@gmail.com>
2019-09-13 14:31:37 +0200

    Updated blogapi

--- lib/services/blogapi.dart
+++ lib/services/blogapi.dart
@@ -10,12 +10,12 @@ import 'requester.dart';
 Future<HttpRequest> createArticle(Article obj) async {
   var url = await buildPath("Blog.API", "article", new List<String>());
 
-  return invokeService("POST", url, true, obj.toJson());
+  return invokeService("POST", url, true, jsonEncode(obj.toJson()));
 }
 
 Future<HttpRequest> updateArticle(Key key, Article obj) async {
   var url = await buildPath("Blog.API", "article", new List<String>());
-  final data = jsonEncode({"Key": key, "Body": obj.toJson()});
+  final data = jsonEncode({"Key": key.toJson(), "Body": obj.toJson()});
 
   return invokeService("PUT", url, true, data);
 }
